a937dcd5d439b07c702e88af88bd8ece1de34dd5
Kai Culemann <culemann@omniscale.de>
2013-08-20 15:05:45 +0200

    Introduced gbi-editor for all maps

---
+++ app/gbi_server/templates/js/translation.js
@@ -0,0 +1,22 @@
+OpenLayers.Lang['en'] = OpenLayers.Util.applyDefaults({
+    //core
+    'Not defined': "{{ _('Not defined') }}",
+
+    // toolbar items
+    'Draw polygon': "{{ _('Draw polygon') }}",
+    'Edit feature': "{{ _('Edit feature') }}",
+    'Delete feature': "{{ _('Delete feature') }}",
+    'Select feature(s)': "{{ _('Select feature(s)') }}",
+    'Copy feature(s)': "{{ _('Copy feature(s)') }}",
+
+    // layer manager widget
+    'activeLayer': "{{ _('Active layer') }}",
+    'backgroundLayerTitle': "{{ _('Background layer') }}",
+    'rasterLayerTitle': "{{ _('Raster layer') }}",
+    'vectorLayerTitle': "{{ _('Vector layer') }}",
+    'noActiveLayer': "{{ _('No active layer')}}",
+
+    // attribute editor widget
+    'noAttributes': "{{ _('No attributes') }}",
+    'sameKeyDifferentValue': "{{ _('Different values for same attribute') }}"
+});
